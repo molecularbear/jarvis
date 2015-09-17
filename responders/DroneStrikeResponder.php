a316d386522f2839f2f4b74c24628db90a6ef514
Ross Davis <molecularbear@gmail.com>
2015-09-17 13:20:18 -0500

    Fleshing out response

--- responders/DroneStrikeResponder.php
+++ responders/DroneStrikeResponder.php
@@ -18,21 +18,24 @@ class DroneStrikeResponder extends Responder
     
     private static $sublocs = array(
         '{fives} clicks {dir} of',
-        'right in the heart of',
         'just {dir} of',
         'on the outskirts of',
         'in a shantytown {dir} of',
-        'in an abandoned mine near',
-        'in downtown'
+        'an abandoned mine near',
+        'downtown',
+        'hocking pirated DVDs on the streets of',
+        'sailing in a hot air balloon over'
     );
     
     private static $locs = array(
-        array('name' => 'Tokyo, Japan', 'latitude' => 35.673343, 'longitude' => 139.710388),
-        array('name' => 'Shanghai, China', 'latitude' => 31.2243489, 'longitude' => 121.4767528),
-        array('name' => 'Mexico City, Mexico', 'latitude' => 19.3907336, 'longitude' => -99.1436127),
+        array('name' => 'Da Nang, Vietnam', 'latitude' => 16.0466742, 'longitude' => 108.206706),
         array('name' => 'Delhi, India', 'latitude' => 28.6454414, 'longitude' => 77.0907573),
-        array('name' => 'Omaha, NE', 'latitude' => 41.2918589, 'longitude' => -96.0812485),
+        array('name' => 'Ho Chi Minh City, Vietnam', 'latitude' => 10.768451, 'longitude' => 106.6943626),
+        array('name' => 'Mexico City, Mexico', 'latitude' => 19.3907336, 'longitude' => -99.1436127),
         array('name' => 'Nashville, TN', 'latitude' => 36.1866405, 'longitude' => -86.7852455),
+        array('name' => 'Omaha, NE', 'latitude' => 41.2918589, 'longitude' => -96.0812485),
+        array('name' => 'Shanghai, China', 'latitude' => 31.2243489, 'longitude' => 121.4767528),
+        array('name' => 'Tokyo, Japan', 'latitude' => 35.673343, 'longitude' => 139.710388)
     );
     
     public function respond($redirect = false) {
@@ -43,13 +46,15 @@ class DroneStrikeResponder extends Responder
         // TODO: add local time?
         // TODO: figure out $this->communication?
         // TODO: auto-parse the matches?
-        /// TODO: add collateral dmg (by the way, ...)
+        // TODO: add more locations
+        // TODO: add collateral dmg (by the way, ...)
         
         $target = $this->matches[1];
         $subloc = self::$sublocs[rand(0, count(self::$sublocs) - 1)];
         $subloc = str_replace('{fives}', rand(1, 4) * 5, $subloc);
         $subloc = str_replace('{dir}', self::$dirs[rand(0, count(self::$dirs) - 1)], $subloc);
         $loc = self::$locs[rand(0, count(self::$locs) - 1)];
+        
         $weatherConfig = array(
             'forecast.io_key' => $this->config['forecast.io_key'],
             'location' => array($loc['latitude'], $loc['longitude'])
@@ -60,16 +65,20 @@ class DroneStrikeResponder extends Responder
         $ball = new EightBallResponder($this->config, $this->communication, array('8ball', array('8ball')), null);
         $permission = $ball->respond();
         if (EightBallResponder::isPositive($permission)) {
-            $mission = ":airplane: ... :three: ... :two: ... :one: ... :boom::boom::boom::boom::boom:\n" .
-                "*$target* was completely vaporized. Misson accomplished!";
+            $booms = str_repeat(':boom:', 5);
+            $mission = ":airplane: ... :three: ... :two: ... :one: ... $booms\n" .
+                ":smiley: We have visual on *$target's* smoldering corpse - mission accomplished!";
         }
         else {
-            $mission = 'Understood, sir. Mission aborted.';
+            $mission = ':disappointed: Understood, sir. Mission aborted.';
         }
         
-        return "Agents have located *$target* $subloc {$loc['name']}.\n" .
-            "Current weather conditions: $forecast\n" .
-            "Do we have permission to neutralize the target?\n" .
+        return ":neutral_face: Sir, agents report a fix on high-value enemy combatant *$target*:\n" .
+            "```\n" .
+            "LOCATION: $subloc {$loc['name']}\n" .
+            "WEATHER: $forecast\n" .
+            "```\n" .
+            ":angry: Do we have permission to neutralize the target?\n" .
             ":guardsman: _{$permission}._\n$mission";
     }
 }
